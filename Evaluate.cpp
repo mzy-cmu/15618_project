52545b352df70782468202947a338c52caecb2c8
mzy-cmu <zhiyingm@andrew.cmu.edu>
2024-11-30 14:39:24 -0500

    interface communication

--- Evaluate.cpp
+++ Evaluate.cpp
@@ -1,8 +1,36 @@
 // evaluate
-vector signal id
-vector bool 2D: 1D: signal id, 2D: testcase value
-Gate -> type, 
+// vector signal id
+// vector bool 2D: 1D: signal id, 2D: testcase value
+// Gate -> type, 
 
-1: 2 3 2: 3 3:
-1:     2: 1 3: 1 2
-1: 0 2: 1 3: 2
+// 1: 2 3 2: 3 3:
+// 1:     2: 1 3: 1 2
+// 1: 0 2: 1 3: 2
+
+// good circuit evaluate -> fault insertion for all signals then evaluate -> change testcase
+
+for testcase
+    for signal_id_list
+        evaluateGate(signal_id)
+
+vector<int> evaluateGates(vector<Gate> gates, vector<int> process_ids, int fault_id) {
+    for(int i = 0; i < process_ids.size(); i++) {
+        int gate_id = process_ids[i];
+        bool gate_value = evaluateGate(gates[gate_id]);
+        if (gate_id == fault_id) {
+            value[gate_id] = !gate_value;
+        } else {
+            value[gate_id] = gate_value;
+        }
+    }
+
+}
+
+vector<int> evaluateGate(Gate gate) {
+    switch(gate.type) {
+        case "AND": 
+            for (int i = 0; i < gate.inputs.size(); i++) {
+                if ()
+            }
+    }
+}
--- ParaFaultSim.cpp
+++ ParaFaultSim.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <string>
 #include <unistd.h>
-#include <queue>
 #include "Circuit.h"
 
 using namespace std;
@@ -29,14 +28,18 @@ int main(int argc, char *argv[]) {
     }
 
     // Data structures
-    vector<int> outputs;                       // Output signal id
+    vector<int> inputs;                        // Input signal ids
+    vector<int> outputs;                       // Output signal ids
     vector<string> signals;                    // Signal id -> name
     unordered_map<string, int> signal_map;     // Name -> signal id
     vector<Gate> gates;                        // Signal id -> gate type (incl INPUT), input ids
-    vector<vector<int>> dependent_signals;      // Siganl id -> dependent output signals
+    vector<vector<int>> dependent_signals;     // Siganl id -> dependent output signals
     vector<int> dependency_degree;             // Signal id -> In-degree of each signal
-    vector<vector<bool>> values;               // Signal id -> [parallel test case values]
+    vector<bool> values;                       // Signal id -> [parallel test case values]
     vector<bool> check_todo;                   // Mark proccessed signals
+
+    vector<bool> output_values;                // Correct output values
+    vector<vector<int>> testcase_faults;       // Testcase -> [stuck-at faults that can be detected, in signal id]
 
     // Parse and init
     try {
@@ -47,6 +50,16 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // for testcase
+    //     evaluate good circuit outputs
+    //     for signal_id_list
+    //         while()
+    //             evaluateGate(list.pop, signal_id)
+
+    // cuda
+    // block: x testcase y fault
+    // thread: wire
+
     // Batch process
     size_t num_signals = signals.size();
     size_t num_signals_accum = 0;
@@ -55,17 +68,26 @@ int main(int argc, char *argv[]) {
         vector<int> signals_todo = popSignals(check_todo, dependent_signals, dependency_degree);
         size_t size = signals_todo.size();
         num_signals_accum += size;
+        
+        // Inputs
+        if (batch_id == 0) {
+            // TODO: Init values input to testcase
+        } 
+        // Evaluate gates
+        else {
+            cout << "Batch " << batch_id << ", "<< size << " wires: ";
+            for (size_t i = 0; i < size; i++) {
+                cout << signals_todo[i] << "(" << signals[signals_todo[i]] << ") ";
+            }
+            cout << "\n";
+        }
 
         batch_id++;
-        cout << "Batch " << batch_id << ", "<< size << " wires: ";
-        for (size_t i = 0; i < size; i++) {
-            cout << signals_todo[i] << "(" << signals[signals_todo[i]] << ") ";
-        }
-        cout << "\n";
     }
 
     if (num_signals_accum != num_signals) {
         cerr << "Error: Proccessed signal count doesn't match\n";
+        return 1;
     }
 
     return 0;
